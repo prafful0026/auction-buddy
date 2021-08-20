cd523a9ee61d721aeeb60eb37ea7e1d1120b6887
Prafful Agrawal <prafful026@gmail.com>
2021-08-21 04:07:34 +0530

    created route for creating new  auction

---
+++ auction-service/src/handlers/createAuction.js
@@ -0,0 +1,30 @@
+import { v4 as uuid } from "uuid";
+import aws from "aws-sdk";
+
+const dynamoDB = new aws.DynamoDB.DocumentClient();
+
+async function createAuction(event, context) {
+  const now = new Date();
+  const { title } = JSON.parse(event.body);
+
+  const newAuction = {
+    id: uuid(),
+    title,
+    status: "OPEN",
+    createdAt: now.toISOString(),
+  };
+
+  await dynamoDB
+    .put({
+      TableName: "AuctionsTable",
+      Item: newAuction,
+    })
+    .promise();
+
+  return {
+    statusCode: 201,
+    body: JSON.stringify(newAuction),
+  };
+}
+
+export const handler = createAuction;
