77e46a9af98d86c862e8dc7532c82d90a5d4fafd
Prafful Agrawal <prafful026@gmail.com>
2021-09-06 12:32:38 +0530

    created common middleware

--- auction-service/src/handlers/createAuction.js
+++ auction-service/src/handlers/createAuction.js
@@ -1,10 +1,7 @@
 import { v4 as uuid } from "uuid";
 import aws from "aws-sdk";
-import middy from "@middy/core";
-import httpJsonBodyParser from "@middy/http-json-body-parser";
-import httpErrorHandler from "@middy/http-error-handler";
-import httpEventNormalizer from "@middy/http-event-normalizer";
 import createError from "http-errors";
+import commonMiddleware from "../utils/commonMiddleware";
 
 const dynamoDB = new aws.DynamoDB.DocumentClient();
 
@@ -36,7 +33,4 @@ async function createAuction(event, context) {
   };
 }
 
-export const handler = middy(createAuction)
-  .use(httpJsonBodyParser())
-  .use(httpEventNormalizer())
-  .use(httpErrorHandler());
+export const handler = commonMiddleware(createAuction);
--- auction-service/src/handlers/getAuctions.js
+++ auction-service/src/handlers/getAuctions.js
@@ -1,8 +1,5 @@
 import aws from "aws-sdk";
-import middy from "@middy/core";
-import httpJsonBodyParser from "@middy/http-json-body-parser";
-import httpErrorHandler from "@middy/http-error-handler";
-import httpEventNormalizer from "@middy/http-event-normalizer";
+import commonMiddleware from "../utils/commonMiddleware";
 import createError from "http-errors";
 
 const dynamoDB = new aws.DynamoDB.DocumentClient();
@@ -28,7 +25,4 @@ async function getAuctions(event, context) {
   };
 }
 
-export const handler = middy(getAuctions)
-  .use(httpJsonBodyParser())
-  .use(httpEventNormalizer())
-  .use(httpErrorHandler());
+export const handler = commonMiddleware(getAuctions);
