b91f8ccf2e5b57b4f84313d71c7834d3bd8eda4f
Prafful Agrawal <prafful026@gmail.com>
2021-09-11 10:12:21 +0530

    added functionality for closing auctions

--- auction-service/src/handlers/processAuctions.js
+++ auction-service/src/handlers/processAuctions.js
@@ -1,7 +1,16 @@
+import createErrors from "http-errors";
 import { getEndedAuctions } from "../utils/getEndedAuctions";
-
+import { closeAuction } from "../utils/closeAuction";
 async function processAuctions(event, context) {
-  const auctionsToClose = await getEndedAuctions();
-  console.log(auctionsToClose);
+  try {
+    const auctionsToClose = await getEndedAuctions();
+    const closePromises = auctionsToClose.map((auction) =>
+      closeAuction(auction)
+    );
+    await Promise.all(closePromises);
+    return { closed: closePromises.length };
+  } catch (error) {
+    throw new createErrors.InternalServerError(error);
+  }
 }
 export const handler = processAuctions;
