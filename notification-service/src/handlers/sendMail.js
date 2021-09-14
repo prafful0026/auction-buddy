ea886d24cbecfff72cf53126bd133cac62c2563a
Prafful Agrawal <prafful026@gmail.com>
2021-09-14 12:02:10 +0530

    integrated nodemailer for sending mails

--- notification-service/src/handlers/sendMail.js
+++ notification-service/src/handlers/sendMail.js
@@ -1,6 +1,29 @@
+import nodemailer from "nodemailer";
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    type: "OAuth2",
+    user: process.env.MAIL_USERNAME,
+    pass: process.env.MAIL_PASSWORD,
+    clientId: process.env.OAUTH_CLIENTID,
+    clientSecret: process.env.OAUTH_CLIENT_SECRET,
+    refreshToken: process.env.OAUTH_REFRESH_TOKEN,
+  },
+});
 async function sendMail(event, context) {
-  console.log(event);
-  return { event };
+  const mailOptions = {
+    from: process.env.MAIL_USERNAME,
+    to: "prafful0026@gmail.com",
+    subject: "Nodemailer Project",
+    text: "Hi from your nodemailer project",
+  };
+  try {
+    const result = await transporter.sendMail(mailOptions);
+    console.log(result);
+    return result;
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 export const handler = sendMail;
