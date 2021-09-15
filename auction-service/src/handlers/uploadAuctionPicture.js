3dfacfa32198b4814cf34a1fb43c3b85ba6949d5
Prafful Agrawal <prafful026@gmail.com>
2021-09-15 13:55:24 +0530

    added image upload to s3 fxn

--- auction-service/src/handlers/uploadAuctionPicture.js
+++ auction-service/src/handlers/uploadAuctionPicture.js
@@ -1,14 +1,35 @@
+import { getAuctionById } from "./getAuction";
+import { uploadPictureToS3 } from "../utils/uploadPictureToS3";
+import middy from "@middy/core";
+import httpErrorHandler from "middy-middleware-json-error-handler";
+import createHttpError from "http-errors";
+import { setAuctionPictureUrl } from "../utils/setAuctionPictureUrl";
+
 export async function uploadAuctionPicture(event, context) {
+  const { id } = event.pathParameters;
+  const { email } = event.requestContext.authorizer;
+  const auction = await getAuctionById(id);
+
+  if (auction.seller !== email) {
+    throw new createHttpError.Forbidden(
+      `You are not the seller of this auction!!`
+    );
+  }
+  const base64 = event.body.replace(/^data:image\/\w+;base64,/, "");
+  const buffer = Buffer.from(base64, "base64");
+  let updatedAuction;
+
   try {
-    return {
-      statusCode: 200,
-      body: JSON.stringify({}),
-    };
+    const pictureUrl = await uploadPictureToS3(auction.id + ".jpg", buffer);
+    updatedAuction = await setAuctionPictureUrl(id, pictureUrl);
   } catch (error) {
     console.log(error);
-    return error;
+    throw new createHttpError.InternalServerError(error);
   }
+  return {
+    statusCode: 200,
+    body: JSON.stringify(updatedAuction),
+  };
 }
 
-export const handler = uploadAuctionPicture;
-
+export const handler = middy(uploadAuctionPicture).use(httpErrorHandler());
