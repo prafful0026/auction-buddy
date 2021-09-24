22b6f07ac23a80c93ba920cc04be8af4b528d614
Praffu Agrawal <prafful026@gmai.com>
2021-09-24 08:17:24 +0530

    added upload image to createAuction

--- auction-service/src/handlers/createAuction.js
+++ auction-service/src/handlers/createAuction.js
@@ -4,27 +4,35 @@ import createError from "http-errors";
 import commonMiddleware from "../utils/commonMiddleware";
 import validator from "@middy/validator";
 import createAuctionSchema from "../utils/schemas/createAuctionSchema";
-
+import { uploadPictureToS3 } from "../utils/uploadPictureToS3";
 const dynamoDB = new aws.DynamoDB.DocumentClient();
 
 async function createAuction(event, context) {
   const now = new Date();
   const { email } = event.requestContext.authorizer;
-  const { title } = event.body;
+  const { title, pictureBase64 } = event.body;
   const endDate = new Date();
   endDate.setHours(now.getHours() + 1);
-  const newAuction = {
-    id: uuid(),
-    title,
-    status: "OPEN",
-    createdAt: now.toISOString(),
-    endingAt: endDate.toISOString(),
-    highestBid: {
-      amount: 0,
-    },
-    seller: email,
-  };
+
+  const base64 = pictureBase64.replace(/^data:image\/\w+;base64,/, "");
+  const buffer = Buffer.from(base64, "base64");
+  let newAuction;
   try {
+    const id = uuid();
+
+    const pictureUrl = await uploadPictureToS3(id + ".jpg", buffer);
+    newAuction = {
+      id,
+      title,
+      status: "OPEN",
+      createdAt: now.toISOString(),
+      endingAt: endDate.toISOString(),
+      highestBid: {
+        amount: 0,
+      },
+      seller: email,
+      pictureUrl,
+    };
     await dynamoDB
       .put({
         TableName: process.env.AUCTIONS_TABLE_NAME,
